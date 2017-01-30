991333da4426a3b609ea243c275889ee99d6b3c1
bkdev98 <bkdev98@gmail.com>
2017-01-30 16:58:25 +0700

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = (props) => {
+  return (
+    <div>
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+      <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+      <IndexLink to="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</IndexLink>
+    </div>
+  );
+};
+
+module.exports = Nav;
